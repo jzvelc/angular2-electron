4f49f7f17e4f37511dcaae3f2a43469fa0964709
Jure Žvelc <jzvelc@gmail.com>
2016-08-02 17:04:18 +0200

    Updates

--- bin/travis.sh
+++ bin/travis.sh
@@ -2,21 +2,19 @@
 
 set -euo pipefail
 
-SWD=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-CWD=$(cd "${SWD}" && cd .. && pwd)
+TRAVIS_OS_NAME=linux
 
-TRAVIS_OS_NAME=osx
-
-#rm -rf ./app/dist ./app/node_modules
-#mkdir -p ./app/dist
-cp package.json src/main.js app/dist
+rm -rf ./app/dist ./app/node_modules
+mkdir -p ./app/dist
+cp ./package.json ./src/main.js app/dist
 
 if [[ "${TRAVIS_OS_NAME}" == "osx" ]]; then
-  #npm install
-  #webpack --config config/webpack.electron.js  --progress --profile --colors --display-error-details --display-cached --bail
+  npm install
+  webpack --config ./config/webpack.electron.js  --progress --profile --colors --display-error-details --display-cached --bail
   ./node_modules/.bin/build -m
 else
+  docker build -t brisket .
   docker run --rm -it -v ${PWD}:/project -v brisket-node-modules:/project/node_modules brisket npm install
-	docker run --rm -it -v ${PWD}:/project -v brisket-node-modules:/project/node_modules brisket webpack --config config/webpack.electron.js  --progress --profile --colors --display-error-details --display-cached --bail
-	docker run --rm -it -v ${PWD}:/project -v brisket-node-modules:/project/node_modules brisket ./node_modules/.bin/build -l
+	docker run --rm -it -v ${PWD}:/project -v brisket-node-modules:/project/node_modules brisket webpack --config ./config/webpack.electron.js  --progress --profile --colors --display-error-details --display-cached --bail
+	docker run --rm -it -v ${PWD}:/project -v brisket-node-modules:/project/node_modules brisket ./node_modules/.bin/build -lw
 fi
